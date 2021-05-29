df829ed0385f813c7290ce5a23e79f82715529f9
Sam Tomashi <samtomashikayembi@yahoo.fr>
2021-05-29 23:29:55 +0300

    Added Android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SalesVisits',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
