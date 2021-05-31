cdb7dcf02f1997de9697958e58509100c6f955a8
Sam Tomashi <samtomashikayembi@yahoo.fr>
2021-05-31 16:22:36 +0300

    add firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://api.cplmobileapp.co.ke/sales-visits/mobile'
+  BASE_URL: 'https://api.cplmobileapp.co.ke/sales-visits/mobile',
+  firebase: {
+    apiKey: "AIzaSyCRE0Y5jrBzOVKUcwxk92sj_JH_FRnQxqE",
+    authDomain: "sales-advisor-866b4.firebaseapp.com",
+    projectId: "sales-advisor-866b4",
+    storageBucket: "sales-advisor-866b4.appspot.com",
+    messagingSenderId: "848859883407",
+    appId: "1:848859883407:web:5396dc2b957b21c85d0dfa"
+  }
 };
