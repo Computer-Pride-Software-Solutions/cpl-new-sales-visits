69d4c58c0b611e9ddd3124dc240eff30332fa874
Sam Tomashi <samtomashikayembi@yahoo.fr>
2021-05-31 16:21:33 +0300

    generate product page

---
+++ src/app/tab4/product-details/product-details.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.page.html',
+  styleUrls: ['./product-details.page.scss'],
+})
+export class ProductDetailsPage implements OnInit {
+  custCode = this.activatedRoute.snapshot.paramMap.get('custCode');
+
+  constructor(private activatedRoute: ActivatedRoute,) { }
+
+  ngOnInit() {
+  }
+
+  getBackButtonText() {
+    const win = window as any;
+    const mode = win && win.Ionic && win.Ionic.mode;
+    return mode === 'ios' ? 'Inbox' : '';
+  }
+
+}
