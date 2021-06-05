7cbf11be7c53a2ede1099f29904479a8f7c9a1a0
Sam Tomashi <samtomashikayembi@yahoo.fr>
2021-06-05 16:02:28 +0300

    Implemented calendar

---
+++ src/app/services/summary/visit-summary.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { environment as envProd} from '../../../environments/environment.prod';
+import { environment as envDev} from '../../../environments/environment';
+import { from, Observable, throwError, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VisitSummaryService {
+  private headers = new HttpHeaders();
+  private param = new HttpParams();
+  currentUser = localStorage.getItem('currentUser');
+
+  constructor(private httpClient: HttpClient) {
+    this.headers = this.headers.set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+    // this.param = this.param.append('currentUser', this.currentUser);
+  }
+
+  getVisitsSummary(custName, visitDate): Observable<any[]> {
+    const endpoint = 'visit-summary';
+    this.param = this.param.append('visitDate', visitDate);
+    let URL = `${envDev.BASE_URL}/${endpoint}/${custName}`;
+    return this.httpClient.get(`${URL}`, {
+      params: this.param,
+      headers : this.headers,
+      withCredentials: true
+    }).pipe(
+      map((data: any[]) => {
+        return data;
+      }), catchError(error => {
+        return throwError(error);
+      })
+    );
+  }
+
+  
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,17 +3,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+
+
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin,
+  timeGridPlugin,
+  listPlugin
+]);
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    FullCalendarModule,
+
   ],
   declarations: [Tab1Page]
 })
