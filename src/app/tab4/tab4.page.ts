0276ee5aa2f76a4e528bfff52efc3c542f29c112
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-04-23 23:03:26 +0300

    implement debounce feature

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,30 +1,37 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IPriceList, IProduct } from '../interfaces/IProducts';
 import { ProductService } from '../services/product/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { ClientService } from '../services/client/client.service';
+import { FormControl, Validators } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit, OnDestroy {
+export class Tab4Page implements OnInit, OnDestroy, AfterViewInit {
   subscription: Subscription = new Subscription();
   isLoading = true;
   pricelists: IPriceList[] = [];
   page:number = 0;
+  itemHint = new FormControl('', [Validators.minLength(3), Validators.min(3)]);
 
   constructor(
     private productService: ProductService,
     private clientService: ClientService,
 
   ) { }
-
-  ngOnInit() {
+  ngAfterViewInit(): void {
     this.getPriceList();
     this.getProducts(this.page);
+    this.searchProducts();
+  }
+
+  ngOnInit() {
+   
   }
 
   ngOnDestroy() {
@@ -35,18 +42,20 @@ export class Tab4Page implements OnInit, OnDestroy {
   // Refreshing page
   doRefresh(event) {
     // Begin async operation
+    this.page = 0;
     this.getProducts(this.page);
     setTimeout(() => {
       // Async operation has ended
       event.target.complete();
-    }, 2000);
+    }, 500);
   }
 
 
   products: IProduct[] = [];
-  getProducts(chunk: number, hint = null){
+  getProducts(chunk: number, hint = ''){
     this.subscription.add(
-      this.productService.getAllProducts(chunk, hint).subscribe((products: IProduct[]) => {
+      this.productService.getAllProducts(chunk, hint)
+      .subscribe((products: IProduct[]) => {
         this.products = products;
         // console.log(products);
         this.isLoading = false;
@@ -56,31 +65,22 @@ export class Tab4Page implements OnInit, OnDestroy {
   }
 
   //check first if the product is already on the frontend before making another API request
-  hint: string = '';
-  searchProducts(event): boolean{
-    this.hint = (event)? event.target.value.toString().toLowerCase(): '';
-    // let regex = new RegExp(this.hint);
-    // let searchedProducts = this.products.filter(product => regex.test(product.ItemName.toLowerCase()));
-
-    // if(searchedProducts.length < 1){
-    //   this.getProducts(this.initialChunk, this.hint);
-    //   return false;
-    // }
-    // this.products = searchedProducts;
-    this.getProducts(this.page, this.hint);
-    return true;
-
+  // hint: string = '';
+  searchProducts(): void{
+    const result = this.itemHint.valueChanges.pipe(debounceTime(600));
+    result.subscribe((hint) => this.getProducts(this.page, hint.toLowerCase()));
   }
 
   loadData(event) {
+    this.products = [];
     this.page = this.page + 1;
     setTimeout(() => {
       event.target.complete();
 
       // App logic to determine if all data is loaded
       // and disable the infinite scroll
-      this.getProducts(this.page, this.hint); // Fetching new orders
-    }, 500);
+      this.getProducts(this.page, this.itemHint.value); // Fetching new orders
+    }, 200);
   }
 
 
