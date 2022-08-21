4af38e1d371e6a07eeef1e87232ce9ca1d74d7ce
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-08-21 12:42:49 +0300

    get company details

---
+++ src/app/services/company/company-details.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { from, Observable, throwError, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyDetailsService {
+  private baseUrl = environment.BASE_URL;
+  private headers = new HttpHeaders();
+
+  constructor(private httpClient: HttpClient) {
+    this.headers = this.headers.set('Content-Type', 'application/json')
+        .set('Accept', 'application/json');
+   }
+
+   getCompanyDetails() {
+    const endpoint = 'clients';
+    let URL = `${this.baseUrl}/${endpoint}`;
+
+    let promise = new Promise((resolve, reject) => {
+      this.httpClient.get(URL, {
+          headers : this.headers,
+          withCredentials: true
+        })
+        .toPromise()
+        .then((res) => resolve(res));
+    });
+    return promise;
+  }
+
+}
