29cba9a61392c7d71c178cae96c9e477fc65821a
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-04-10 17:52:29 +0300

    fecth all projects

--- src/app/services/client/client.service.ts
+++ src/app/services/client/client.service.ts
@@ -105,6 +105,21 @@ export class ClientService {
     );
   }
 
+  getAllProjects(): Observable<any[]>{
+        const endpoint = 'projects';
+        return this.httpClient.get(`${this.baseUrl}/${endpoint}`, {
+        headers : this.headers,
+        withCredentials: true
+      }).pipe(
+        map((data: any[]) =>{
+          return data;
+        }), catchError(error => {
+          return throwError(error);
+        })
+      );
+
+  }
+
   mapClient(custCode: string, latlon: string){
     const endpoint = 'client';
     return this.httpClient.put(`${this.baseUrl}/${endpoint}/${custCode}`, {
